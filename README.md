66f4c0376362cab0889512a8c9e8e6dee0dc3398
xand3r8 <angel.arias173@gmail.com>
2024-08-12 00:24:15 -0700

    real initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# How to use
+1. Ensure that the script is inside of a valid minecraft directory, (where your mods and configs files live) if the folders don't exist you can simply create them.
+2. You can run the mod directly using ```python3 main.py``` or running the exectuable if you dowloaded from releases, (make sure the executable has execute permission)
+3. On first run the script will create an ini file inside of the mods folder called "msmh-instance.ini" and then exit. Open this ini file, by default it will contain my repos URL (modpacks and config), you can delete these and add your desired hosting/server URL(s), optionally you can also add a start command to auto run the client after mod check and updating has completed
+4. Run the script or executable again to begin mod/config synchronization.
+
+# How it works
+The script simply makes a list of all mods in the mods directory, it then downloads and fetches the mod manifest from the provided manifest url and compares the two lists. If there is any mismatch or missing mod files the provided server url is used to download the modpack, then it is is extracted from the pack and installed directly onto the mods folder. On any mod changes, the configuration folder is also updated alongside; typically there are a lot of configuration files that move/change when deleting/adding/updating mods and would add complexity to keep track of, since these files are usually not too crucial (depending on the server), the configuration folder is downloaded entirely and replaced in the proccess.
+
+# For server owners
+If you are on the other end, the server hoster, you need to make sure that your server has the necessary mods and config files in correct folder hierarchy in order for the script to correctly identifiy the files needed. When connecting to a server, the script will initially look for one file: manifest.txt. This file should also have an updated list of all server mods for update mod comparison. The link to manifest.txt should be provided directly in the ini file, this is to prevent unecessary downloading if there are no mod updates.
+
+```
+(your_modpack.zip) contents:
+mods <-- from your mc server
+config <-- from your mc server
+```
+Whenever you update your server mods/config files, you need to ensure to also update the manifest.txt file, alongside the modpack.zip. You can use this repo's file architecture as a template.
+```
+somewhere in your server...
+https://yourserver.com/msm-helper/manifest.txt
+```
+
+You can use the script to create a manifest.txt list, make sure you run it where your server mods list, it will create the text file in the current working directory.
+To create a manifest.txt use:
+```
+python3 main.py -m
+```
---
+++ src/msmh.py
@@ -0,0 +1,201 @@
+""" 
+    Minecraft Server Mods Helper
+    Copyright (C) 2024 ALEX A https://repo.alexarias.dev/xander8/msm-helper
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import platform
+import shutil
+import configparser
+import requests
+import argparse
+import zipfile
+import os
+import sys
+
+VERSION = "0.1"
+USR_OS = "mac" if platform.system().lower() == "darwin" else platform.system().lower()
+CONFIG_FILE_PATH = "mods/msmh-instance.ini"
+UPDATED_MODS_FOLDER = "mcmh-updater/mods"
+UPDATED_CONFIG_FOLDER = "mcmh-updater/config"
+
+print(f"Minecraft Server Mods Helper {VERSION} by xander8")
+print(f"Detected OS: {USR_OS}")
+
+class MSMHelper:
+    def __init__(self):
+        self.modpack_url = ""
+        self.manifest_url = ""
+        self.start_command = ""
+
+    def create_instance(self):
+        if not os.path.isfile(CONFIG_FILE_PATH):
+            config = configparser.ConfigParser()
+            config["Instance"] = {
+            "modpack_url": "https://github.com/xand3r8/msm-helper/modpack.zip",
+            "manifest_url": "https://github.com/xand3r8/msm-helper/manifest.txt",
+            "start_command": "",
+        }
+            with open(CONFIG_FILE_PATH, "w") as f:
+                config.write(f)
+            print("Created a new configuration file inside of the mods folder. Please edit before executing MSM-Helper again.")
+            sys.exit(0)
+        else:
+            print("Found a configuration file!")
+    
+    def check_if_server_exist(self):
+        if not os.path.exists("mods") or not os.path.exists("config"):
+            print("Not inside of a minecraft directory! Ensure the script is executed inside of a valid minecraft directory!")
+            return sys.exit(1)
+
+        if not os.path.exists(CONFIG_FILE_PATH):
+            print("Did not find instance lock file! Creating one...")
+            self.create_instance()
+        
+        self.read_config_file()
+
+    def read_config_file(self):
+        try:
+            with open(CONFIG_FILE_PATH, "r") as f:
+                config_file = f.readlines()
+
+            config = configparser.ConfigParser()
+            config.read_string("".join(config_file))
+            section = config.sections()
+            
+            self.modpack_url = config.get(section[0], "modpack_url")
+            self.manifest_url = config.get(section[0], "manifest_url")
+            self.start_command = config.get(section[0], "start_command")
+
+            self.get_mod_manifest(self.manifest_url)
+
+        except FileNotFoundError:
+            print("Did not find instance config file!")
+            sys.exit(1)
+
+    def get_mod_manifest(self, manifest_url: str) -> list:
+        """
+        fetches the manifest.txt file from the server 
+        """
+        response = requests.get(manifest_url, stream=True)
+
+        if response.status_code == 200:
+            with open("manifest.txt", 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            print(f"File downloaded successfully: manifest.txt")
+            self.read_and_validate_mods()
+        else:
+            print(f"Failed to download file: {response.status_code}")
+    
+    @staticmethod
+    def create_mod_manifest():
+        """
+        creates a manifest.txt file containing a list of
+        all mods in mods folder
+        """
+        mods = os.listdir("mods")
+        with open("manifest.txt", "w") as f:
+            f.write(str(mods))
+        print("Successfully created manifest.txt")
+
+    def download_pack(self, url):
+        """
+        downloads the modpack from the provided url
+        """
+        response = requests.get(url, stream=True)
+
+        if response.status_code == 200:
+            with open("modpack.zip", 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            print(f"File downloaded successfully: modpack.zip")
+            self.unpack_modpack()
+        else:
+            print(f"Failed to download file: {response.status_code}")
+    
+    def unpack_modpack():
+        """
+        Unzip a ZIP file and extract its contents to the specified directory.
+
+        :param zip_file_path: Path to the ZIP file
+        :param extract_to_dir: Directory where the files will be extracted
+        """
+        with zipfile.ZipFile("modpack.zip", 'r') as zip_ref:
+            zip_ref.extractall("mcmh-updater")
+        print(f"Successfully unpacked modpack archive...")
+
+
+    def install_mod(self, name):
+        """
+        updates the necessary mods
+        :param name: # the filename of the mod to install
+        """
+        for mod in os.listdir(UPDATED_MODS_FOLDER):
+            if name in mod:
+                os.remove(f"mods/{mod}")
+                shutil.move(f"{UPDATED_MODS_FOLDER}/{name}", "mods")
+                print(f"Removed mod {mod}, and updated it with {name}")
+
+    def update_config_files(self):
+        """
+        updates the config folder with the one provided
+        by the pack
+        """
+        os.remove("configs")
+        os.mkdir("configs")
+        shutil.move(f"{UPDATED_MODS_FOLDER}/configs", "configs")
+        print("Successfully updated config folder!")
+
+    def read_and_validate_mods(self, server_mods):
+        """
+        validates mods with the server and ensures they are updated
+        """
+        try:
+            with open("manifest.txt", "r") as f:
+                server_mods = f.readlines()
+        except FileNotFoundError:
+            print("Did not find manifest.txt during mod comparison! Make sure it wasn't deleted or moved. Exiting.")
+            sys.exit(0)
+        
+        local_mods = os.listdir("mods")
+
+        for mod in local_mods:
+            for s_mod in server_mods:
+                try:
+                    if mod != s_mod:
+                        self.install_mod(s_mod)
+                except KeyError:
+                    print("Found new mod(s) not part of the local manifest... Updating..")
+                    self.install_mod(s_mod)
+            break
+        self.update_config_files()
+        print("Done! All mods have been updated to their latest version. You can close this window and launch Minecraft")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Minecraft Server Mod Helper")
+    parser.add_argument(
+        "-m",
+        "-manifest",
+        action="store_true",
+        help="Create a text file containing a list of mods"
+    )
+    args = parser.parse_args()
+    msm = MSMHelper()
+
+    if args.m:
+        msm.create_mod_manifest()
+        sys.exit(0)
+
+    msm.check_if_server_exist()
